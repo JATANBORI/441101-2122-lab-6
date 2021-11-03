cab33048c5c411d0952b39382a0083d6db7033a7
JATANBORI <71958111+JATANBORI@users.noreply.github.com>
2021-11-03 09:13:40 +0000

    Update Program.cs

--- L6.3-TotalBirths/Program.cs
+++ L6.3-TotalBirths/Program.cs
@@ -6,7 +6,6 @@ namespace L6._3_TotalBirths
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Total Drug-Related Deaths England and Wales (2001 -2020).");
             
             int[] annualBirths = {1851, 1669, 1480, 1565, 1675, 1637, 1809, 2004, 1976, 1903, 
             1737, 1636, 1957, 2248, 2479, 2596, 2503, 2917, 2883, 2996};
